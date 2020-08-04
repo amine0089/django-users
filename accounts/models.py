6a4874c24044c2e23658f7e4b5516d82af6c7230
amine0089 <mohamedamine371@gmail.com>
2020-08-04 23:14:20 +0200

    create profile signals

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+class Profile(models.Model):
+	user = models.OneToOneField(User,on_delete = models.CASCADE)
+	Phone = models.CharField(max_length=50 , null=True , blank=True)
+	adresse = models.CharField(max_length=50 , null=True , blank=True)
+
+	def __str__(self):
+		return str(self.user)
+@receiver(post_save,sender = User)
+def create_user_profile(sender,instance,created,**kwargs):
+	if created:
+		Profile.objects.create(user = instance)
+
